fc2779004ad6d38733f925c39c79c3d53e1306fd
Davison <wonsurum77@gmail.com>
2025-06-13 18:45:23 +0100

    Test with placeholder image

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
         <ProfileCard
           name="Wonsurum Davison"
-          image="/Profile2.jpg"
+          image="https://via.placeholder.com/300"
           bio="“I’m a front-end web developer who transforms ideas into user-friendly, responsive web experiences. I have a strong foundation in HTML and CSS, along with extensive experience working with modern JavaScript frameworks like React, Vue, and Node.js.”"
         />
       </div>
