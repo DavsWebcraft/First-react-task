855aad3c1eef6d88935d9f65748547e658605262
Davison <wonsurum77@gmail.com>
2025-06-13 18:55:50 +0100

    Fix image height and layout issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
         <ProfileCard
           name="Wonsurum Davison"
-          image="https://via.placeholder.com/300"
+          image="/Profile2.jpg"
           bio="“I’m a front-end web developer who transforms ideas into user-friendly, responsive web experiences. I have a strong foundation in HTML and CSS, along with extensive experience working with modern JavaScript frameworks like React, Vue, and Node.js.”"
         />
       </div>
